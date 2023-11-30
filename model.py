3304dab20b67d4cfa59be6f65dab5536b52cbcfe
Vedic Panda <vpanda6@gatech.edu>
2023-11-30 14:50:32 -0500

    updated model to regession and output model performance metrics

--- model.py
+++ model.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
 import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.metrics import mean_squared_error
-# import tensorflow as tf
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 
 
 # Load the CSV file into a DataFrame
@@ -47,59 +49,61 @@ pollution_data_2016 = df[~(df['Name'].isin(relevant_indicators)) & (df['Time Per
 
 # Merge the two DataFrames based on 'Geo Place Name' and 'Time Period'
 merged_data = pd.merge(pollution_data_2005, vehicle_data, on=['Geo Place Name', 'Time Period'], how='left', suffixes=('_pollution', '_vehicle'))
-print(merged_data.columns)
+#print(merged_data.columns)
 test_pivot = pd.pivot_table(merged_data, values="Data Value_vehicle", index = ["Name_pollution","Geo Place Name", "Time Period" ,  "Data Value_pollution"], columns="Name_vehicle").fillna(0)
 # test_pivot = pd.pivot_table(merged_data, values="Data Value_pollution", index = ["Name_pollution","Geo Place Name", "Time Period" , "Data Value_vehicle"  ], columns="Name_vehicle").fillna(0)
 # test_pivot = pd.pivot(merged_data, columns=["Name_vehicle"], values="Data Value_vehicle")
 # test_pivot.to_csv("pivot.csv")
-print(test_pivot)
+#print(test_pivot)
 # print(test_pivot["Data Value_pollution"])
-print(test_pivot.columns)
-merged_data.to_csv('Test.csv')
+#print(test_pivot.columns)
+#merged_data.to_csv('Test.csv')
 new_df = pd.read_csv("pivot.csv")
 print(new_df.columns)
 
 #preprocessing almost done. Once we have it in the correct format, we will use this boiler plate code to make our predictions:
 
 # Specify the features and target column
-# feature_columns = ['feature1', 'feature2', 'feature3']  # Add your selected feature columns
-# target_column = 'target_column'
+feature_columns = ['Annual vehicle miles traveled', 'Annual vehicle miles travelled (cars)', 'Annual vehicle miles travelled (trucks)']
+target_column = 'Data Value_pollution'
 
-# # Extract features and target
-# X = merged_data[feature_columns]
-# y = merged_data[target_column]
+# Extract features and target
+X = new_df[feature_columns]
+y = new_df[target_column]
 
-# # Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# # Standardize the data
-# scaler = StandardScaler()
-# X_train_scaled = scaler.fit_transform(X_train)
-# X_test_scaled = scaler.transform(X_test)
+# Standardize the data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-# # Build the neural network model
-# model = Sequential()
+# Specify the output layer for regression
+model = Sequential()
+input_dim = X_train_scaled.shape[1]
+model.add(Dense(units=input_dim, input_dim=input_dim, activation='relu'))
+model.add(Dense(units=10, activation='relu'))
+model.add(Dense(units=1))  # No activation function for regression
 
-# # Input layer
-# input_dim = X_train_scaled.shape[1]
-# model.add(Dense(units=input_dim, input_dim=input_dim, activation='relu'))
+# Compile and train the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(X_train_scaled, y_train, epochs=50, batch_size=32, validation_split=0.2, verbose=2)
 
-# # Hidden layer
-# model.add(Dense(units=10, activation='relu'))
+# Make predictions
+predictions = model.predict(X_test_scaled)
 
-# # Output layer (1 unit for regression, no activation function)
-# model.add(Dense(units=1))
+# Evaluate the model
+mse = mean_squared_error(y_test, predictions)
+print(f'Mean Squared Error: {mse}')
 
-# # Compile the model
-# model.compile(optimizer='adam', loss='mean_squared_error')
+r_squared = r2_score(y_test, predictions)
+print(f'R-squared: {r_squared}')
 
-# # Train the model
-# model.fit(X_train_scaled, y_train, epochs=50, batch_size=32, validation_split=0.2, verbose=2)
-
-# # Make predictions
-# predictions = model.predict(X_test_scaled)
-
-# # Evaluate the model
-# mse = mean_squared_error(y_test, predictions)
-# print(f'Mean Squared Error: {mse}')
-
+plt.plot(y_test.values, label='Actual Values', linestyle='-', marker='o')
+plt.plot(predictions, label='Predicted Values', linestyle='-', marker='o')
+plt.xlabel('Sample Index')
+plt.ylabel('Values')
+plt.title('Actual vs Predicted Values')
+plt.legend()
+plt.show()
